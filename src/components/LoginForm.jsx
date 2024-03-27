3f2d620a81694e3119c6153f47d67ce440399298
dziubenzo <dziubenzo@gmail.com>
2024-03-27 14:38:22 +0100

    Build LoginForm component

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,5 +1,29 @@
 function LoginForm() {
-  return <h1>Some login form</h1>;
+  return (
+    <div className="login-form">
+      <form method="post">
+        <label htmlFor="username">Username</label>
+        <input
+          type="text"
+          name="username"
+          id="username"
+          minLength="1"
+          maxLength="16"
+          required
+        />
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          name="password"
+          id="password"
+          minLength="1"
+          maxLength="16"
+          required
+        />
+        <button type="submit">Log In</button>
+      </form>
+    </div>
+  );
 }
 
 export default LoginForm;

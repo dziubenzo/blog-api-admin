c1f38c412932ddc28621683bfc367b5dc9b7e647
dziubenzo <dziubenzo@gmail.com>
2024-03-27 19:50:13 +0100

    Implement log in functionality

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,19 @@
+import { Outlet } from 'react-router-dom';
+import { useState } from 'react';
 import Footer from './Footer';
-import Router from './Router';
 
 function App() {
+  const [isAuth, setIsAuth] = useState(false);
+
   return (
     <>
       <header>
         <h1>Blog API</h1>
         <h3>Admin Page</h3>
+        {isAuth ? <h3>Logged In</h3> : <h3>Logged Out</h3>}
       </header>
       <main>
-        <Router />
+        <Outlet context={{ isAuth, setIsAuth }} />
       </main>
       <footer>
         <Footer />
--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,7 +1,51 @@
+import { useRef, useState } from 'react';
+import { useNavigate, useOutletContext } from 'react-router-dom';
+
 function LoginForm() {
+  const formRef = useRef(null);
+  const navigate = useNavigate();
+  const { setIsAuth } = useOutletContext();
+
+  // State for showing errors
+  const [isError, setIsError] = useState(false);
+
+  // Log in user
+  async function logIn(event) {
+    event.preventDefault();
+    // Retrieve and prepare form data
+    const formData = new FormData(event.target);
+    const newCommentData = {
+      username: formData.get('username'),
+      password: formData.get('password'),
+    };
+    try {
+      const res = await fetch(`http://localhost:3000/users/login`, {
+        method: 'POST',
+        body: JSON.stringify(newCommentData),
+        headers: { 'Content-Type': 'application/json' },
+      });
+      // Show error message for 3 seconds and clear form fields if credentials incorrect
+      if (!res.ok) {
+        setIsError(true);
+        setTimeout(() => {
+          setIsError(false);
+        }, 3000);
+        formRef.current.reset();
+        return;
+      }
+      // Store token in localStorage and authenticate user
+      const { token } = await res.json();
+      localStorage.setItem('blogAPIToken', token);
+      setIsAuth(true);
+      // Redirect to Dashboard page
+      return navigate('/dashboard');
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
   return (
     <div className="login-form">
-      <form method="post">
+      <form ref={formRef} method="post" onSubmit={logIn}>
         <label htmlFor="username">Username</label>
         <input
           type="text"
@@ -21,6 +65,7 @@ function LoginForm() {
           required
         />
         <button type="submit">Log In</button>
+        {isError && <h3>Incorrect credentials</h3>}
       </form>
     </div>
   );
